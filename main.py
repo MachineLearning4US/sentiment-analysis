0ada6fd86283cfd21e467f13df6824092a8897e6
foutejordan <2ftech20@gmail.com>
2024-02-25 20:13:40 +0100

    added script for scraping

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# launch the server in this file
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, log_level="info")
